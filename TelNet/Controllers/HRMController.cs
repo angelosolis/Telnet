050c858080ae7cfd7285b4f68f54630edb999255
Angelo <solisangelo882@gmail.com>
2024-05-09 07:26:09 +0800

    Smal opdate

---
+++ TelNet/Controllers/HRMController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TelNet.Controllers
+{
+    public class HRMController : Controller
+    {
+        // GET: HRM
+        public ActionResult HRMDashboard()
+        {
+            return View();
+        }
+    }
+}
